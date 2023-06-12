28f3b0598e64e681a764c2e2d56637171470a568
Wirstblase <mariussuflea13@icloud.com>
2023-06-12 20:41:16 +0300

    Initial Commit

---
+++ Web3ProjectManagement/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Web3ProjectManagement
+//
+//  Created by Suflea Marius on 12.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
